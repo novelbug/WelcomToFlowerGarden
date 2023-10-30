fc0e908f131bde1fa60637af56449b2de615d762
novebug <zita1122334@gmail.com>
2023-10-31 00:48:50 +0900

    IngameSetting

---
+++ Assets/01.Script/GameManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Text TalkBar;
+
+    
+
+
+    public void WakeUpHero()
+    {
+        
+    }
+}
